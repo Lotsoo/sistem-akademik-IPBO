b05969e35e91be101002c535b4159b65da4568fa
lotsoo <braindeathrama@gmail.com>
2025-10-17 07:53:25 +0100

    feat(model) add file varMataKuliah

---
+++ src/model/varMataKuliah.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class varMataKuliah {
+    private String kodeMTK, namaMTK, kodePrasyarat;
+    private int sks;
+
+    public String getKodeMTK() {
+        return kodeMTK;
+    }
+
+    public void setKodeMTK(String kodeMTK) {
+        this.kodeMTK = kodeMTK;
+    }
+
+    public String getNamaMTK() {
+        return namaMTK;
+    }
+
+    public void setNamaMTK(String namaMTK) {
+        this.namaMTK = namaMTK;
+    }
+
+    public String getKodePrasyarat() {
+        return kodePrasyarat;
+    }
+
+    public void setKodePrasyarat(String kodePrasyarat) {
+        this.kodePrasyarat = kodePrasyarat;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+}
